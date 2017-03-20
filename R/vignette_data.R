456809ee168a82f601512ab285e032b9c9557285
nate-d-olson <nathandavidolson@gmail.com>
2017-03-20 12:32:00 -0400

    underscore being deprecated in metagenomeSeq

--- R/vignette_data.R
+++ R/vignette_data.R
@@ -2,14 +2,14 @@
 vignette_assay_data <- function(){
     system.file("extdata", "mock_counts.csv",
                 package="metagenomeFeatures") %>%
-        metagenomeSeq::load_meta(sep = ",")
+        metagenomeSeq::loadMeta(sep = ",")
 }
 
 
 vignette_pheno_data <- function(){
     system.file("extdata", "mock_sample_data.csv",
                 package="metagenomeFeatures")  %>%
-        read.csv(row.names = 1, stringsAsFactors = F) %>%
+        read.csv(row.names = 1, stringsAsFactors = FALSE) %>%
         Biobase::AnnotatedDataFrame()
 }
 
